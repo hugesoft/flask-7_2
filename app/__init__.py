f5cd528a08de0a2093c77b3ac183d1b486e62a9d
root <hugesoft@126.com>
2015-08-27 23:27:07 +0800

    修改：     app/__init__.py 	修改：     app/__init__.pyc 	修改：     app/main/__init__.pyc 	修改：     app/main/views.py 	修改：     app/main/views.pyc 	新文件：   app/templates/base.html 	新文件：   app/templates/index.html 	新文件：   app/templates/user.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
+    
+    bootstrap.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
+    app.debug = True
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 from . import main
 
 @main.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+#'<h1>Hello World!test</h1>'
+
+@main.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
