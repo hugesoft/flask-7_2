f8e397b84b77c426e7f204891b68b9803caf788d
root <hugesoft@126.com>
2015-09-03 23:23:03 +0800

    修改：     app/__init__.py 	修改：     app/__init__.pyc 	新文件：   app/auth/__init__.py 	新文件：   app/auth/__init__.pyc 	新文件：   app/auth/views.py 	新文件：   app/auth/views.pyc 	修改：     app/main/forms.py 	修改：     app/main/forms.pyc 	修改：     app/models.py 	修改：     app/models.pyc 	新文件：   app/templates/auth/login.html 	新文件：   app/templates/back.html

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,5 +4,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 class NameForm(Form):
-    name = StringField(u'请输入你的姓名', validators=[Required()])
+    name = StringField(u'请输入你的姓名',  validators=[Required()])
+    name.size = 12
     submit = SubmitField(u'提交')
