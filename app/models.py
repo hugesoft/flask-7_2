fb210b13b64b7544bc045e03a4891c8ae9a4451e
root <hugesoft@126.com>
2015-09-06 00:10:36 +0800

    修改：     app/auth/forms.py 	修改：     app/auth/forms.pyc 	修改：     app/auth/views.py 	修改：     app/auth/views.pyc 	修改：     app/models.py 	修改：     app/models.pyc 	新文件：   app/templates/auth/email/2.html 	新文件：   app/templates/auth/email/2.txt 	新文件：   app/templates/auth/email/confirm.html 	新文件：   app/templates/auth/email/confirm.txt 	修改：     app/templates/auth/login.html	新文件：   app/templates/auth/register.html 	修改：     data-dev.sqlite

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from . import db
 from . import login_manager
 from flask.ext.login import UserMixin
@@ -23,6 +25,24 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64), unique = True, index = True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed =True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
